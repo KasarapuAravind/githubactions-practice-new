09cb13bcebb61002b58fd278401dcf2f3aa02ae4
Kasarapu Aravind <splendidaravind552@gmail.com>
2025-03-29 02:03:05 -0500

    add workflows

---
+++ terraform/environments/dev/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "dev-aravind-terraform-state-bucket"
+    key = "s3/dev/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "dev-terraform-locks" 
+  }
+}

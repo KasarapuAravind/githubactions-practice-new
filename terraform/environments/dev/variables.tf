ef0370d545191cbade11bab3cce3903f8c9c734b
Kasarapu Aravind <splendidaravind552@gmail.com>
2025-03-30 15:53:52 -0500

    add workflows

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -13,20 +13,3 @@ variable "region" {
 }
 
 variable "env" {}
-variable "vpc_id" {}
-variable "public_subnet_ids" {
-  type = list(string)
-}
-variable "ecs_cluster_name" {}
-variable "instance_type" {}
-variable "instance_profile_name" {}
-variable "task_execution_role_arn" {
-  type        = string
-  description = "IAM role ARN for ECS task execution"
-}
-
-variable "app_image_url" {
-  type        = string
-  description = "ECR image to run inside ECS container"
-}
-
---
+++ terraform/modules/ecs/variables.tf
@@ -0,0 +1,4 @@
+variable "env" {
+  description = "Environment (dev, test, prod)"
+  type        = string
+}

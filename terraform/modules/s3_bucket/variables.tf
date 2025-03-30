35c617e448cfabd07a8a4989d5e1b885bb6f1dcd
Kasarapu Aravind <splendidaravind552@gmail.com>
2025-03-30 14:56:30 -0500

    add workflows

--- terraform/modules/s3_bucket/variables.tf
+++ terraform/modules/s3_bucket/variables.tf
@@ -7,3 +7,32 @@ variable "tags" {
     default = {}
 }
 
+variable "env" {
+  description = "Environment (dev, test, prod)"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID for networking"
+  type        = string
+}
+
+variable "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  type        = list(string)
+}
+
+variable "ecs_cluster_name" {
+  description = "ECS cluster name"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for ECS"
+  type        = string
+}
+
+variable "instance_profile_name" {
+  description = "IAM instance profile name for ECS EC2 instances"
+  type        = string
+}
